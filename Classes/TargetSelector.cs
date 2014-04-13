4ecf28f5b336c095ec5a9466299fbe8c6130d492
Andyi19 <andy200713@web.de>
2014-04-13 21:11:10 +0200

    Updated

--- Classes/TargetSelector.cs
+++ Classes/TargetSelector.cs
@@ -1,298 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Math;
+using System.Drawing;
+using System.Windows.Input;
 
-public enum TargetingMode
+namespace TF
 {
-    LowHP,
-    MostAD,
-    MostAP,
-    Closest,
-    NearMouse,
-    AutoPriority,
-}
-
-class TargetSelector
-{
-    private float _range;
-    private TargetingMode _mode;
-    public Unit target;
-    private bool drawcircle = false;
-    private bool update = true;
-
-    public TargetSelector(float range, TargetingMode mode)
+    class Program
     {
-        _range = range;
-        _mode = mode;
-
-        Game.OnGameUpdate += Game_OnGameUpdate;
-        Drawing.OnDraw += Drawing_OnDraw;
-    }
-
-    void Drawing_OnDraw(EventArgs args)
-    {
-        if (drawcircle == true && target != null && target.IsVisible == true && target.IsVisible == true && target.IsEnemy == true)
+        static void Main(string[] args)
         {
-            Drawing.DrawCircle(target.Position, 125, System.Drawing.Color.White);
-        }
-    }
+            GameMode gamemode = GameMode.Connecting;
 
-    private static double lasttick = 0;
-
-    void Game_OnGameUpdate(EventArgs args)
-    {
-        if (Environment.TickCount > lasttick + 500)
-        {
-            lasttick = Environment.TickCount;
-            if (update == true)
+            while (gamemode != GameMode.Running)
             {
-                switch (_mode)
+                gamemode = Game.Mode;
+            }
+
+            if (ObjectManager.Player.BaseSkinName == "TwistedFate")
+            {
+                Game.OnWndProc += Game_OnWndProc;
+                Game.PrintChat(">> TF by Andyi loaded <<");
+            }
+        }
+
+        private static int lastW;
+
+        static void Game_OnWndProc(WndEventArgs args)
+        {
+            if (MenuGUI.IsChatOpen == true)
+                return;
+
+            if (args.Msg == 0x100)
+            {
+                switch (args.WParam)
                 {
-                    case TargetingMode.LowHP:
-                        target = GetLowestHPTarget();
+                    case 'E':
+                        GetCard(CardType.Blue);
+                        args.Process = false;
                         break;
-                    case TargetingMode.MostAD:
-                        target = MostAD();
+                    case 'T':
+                        GetCard(CardType.Gold);
+                        args.Process = false;
                         break;
-                    case TargetingMode.MostAP:
-                        target = MostAP();
-                        break;
-                    case TargetingMode.Closest:
-                        target = Closest();
-                        break;
-                    case TargetingMode.NearMouse:
-                        target = NearMouse();
-                        break;
-                    case TargetingMode.AutoPriority:
-                        target = AutoPriority();
+                    case 'G':
+                        GetCard(CardType.Red);
+                        args.Process = false;
                         break;
                 }
             }
         }
-    }
 
-    private Unit GetLowestHPTarget()
-    {
-        Unit lowesttarget = null;
-        foreach (Unit target in Unit.Heroes)
+        private enum CardType
         {
-            if (target != null && target.IsValid == true && target.IsEnemy == true && target.IsDead == false && target.IsVisible == true && Vector3.Distance(Unit.Player.Position, target.Position) <= _range)
-            {
-                if (lowesttarget == null)
-                {
-                    lowesttarget = target;
-                }
-                else
-                {
-                    if (target.Health < lowesttarget.Health)
-                    {
-                        lowesttarget = target;
-                    }
-                }
-            }
+            Blue,
+            Gold,
+            Red,
         }
-        return lowesttarget;
-    }
 
-    private Unit MostAD()
-    {
-        Unit mostad = null;
-        foreach (Unit target in Unit.Heroes)
+        private static void GetCard(CardType type)
         {
-            if (target != null && target.IsValid == true && target.IsEnemy == true && target.IsDead == false && target.IsVisible == true && Vector3.Distance(Unit.Player.Position, target.Position) <= _range)
+            if (ObjectManager.Player.Spellbook.CanUseSpell(SpellSlot.W) == SpellState.Ready)
             {
-                if (mostad == null)
+                if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name == "PickACard" && Environment.TickCount > lastW + 3000)
                 {
-                    mostad = target;
+                    ObjectManager.Player.Spellbook.CastSpell(SpellSlot.W);
+                    lastW = Environment.TickCount;
                 }
-                else
+                switch (type)
                 {
-                    if (target.BaseAttackDamage + target.FlatPhysicalDamageMod < mostad.BaseAttackDamage + mostad.FlatPhysicalDamageMod)
-                    {
-                        mostad = target;
-                    }
-                }
-            }
-        }
-        return mostad;
-    }
-
-    private Unit MostAP()
-    {
-        Unit mostap = null;
-        foreach (Unit target in Unit.Heroes)
-        {
-            if (target != null && target.IsValid == true && target.IsEnemy == true && target.IsDead == false && target.IsVisible == true && Vector3.Distance(Unit.Player.Position, target.Position) <= _range)
-            {
-                if (mostap == null)
-                {
-                    mostap = target;
-                }
-                else
-                {
-                    if (target.FlatMagicDamageMod < mostap.FlatMagicDamageMod)
-                    {
-                        mostap = target;
-                    }
-                }
-            }
-        }
-        return mostap;
-    }
-
-    private Unit Closest()
-    {
-        Unit closest = null;
-        foreach (Unit target in Unit.Heroes)
-        {
-            if (target != null && target.IsValid == true && target.IsEnemy == true && target.IsDead == false && target.IsVisible == true && Vector3.Distance(Unit.Player.Position, target.Position) <= _range)
-            {
-                if (closest == null)
-                {
-                    closest = target;
-                }
-                else
-                {
-                    if (Vector3.Distance(Unit.Player.Position, target.Position) < Vector3.Distance(Unit.Player.Position, closest.Position))
-                    {
-                        closest = target;
-                    }
-                }
-            }
-        }
-        return closest;
-    }
-
-    private Unit NearMouse()
-    {
-        Unit nearmouse = null;
-        foreach (Unit target in Unit.Heroes)
-        {
-            if (target != null && target.IsValid == true && target.IsEnemy == true && target.IsDead == false && target.IsVisible == true && Vector3.Distance(Unit.Player.Position, target.Position) <= _range)
-            {
-                if (nearmouse == null)
-                {
-                    nearmouse = target;
-                }
-                else
-                {
-                    if (Vector3.Distance(Game.CursorPos, target.Position) < Vector3.Distance(Game.CursorPos, nearmouse.Position))
-                    {
-                        nearmouse = target;
-                    }
-                }
-            }
-        }
-        return nearmouse;
-    }
-
-    private static string[] ap = new string[] { "Ahri", "Akali", "Anivia", "Annie", "Brand", "Cassiopeia", "Diana", "FiddleSticks", "Fizz", "Gragas", "Heimerdinger", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Leblanc", "Lissandra", "Lux", "Malzahar", "Mordekaiser", "Morgana", "Nidalee", "Orianna", "Ryze", "Sion", "Swain", "Syndra", "Teemo", "TwistedFate", "Veigar", "Viktor", "Vladimir", "Xerath", "Ziggs", "Zyra", "Velkoz" };
-    private static string[] sup = new string[] { "Blitzcrank", "Janna", "Karma", "Leona", "Lulu", "Nami", "Sona", "Soraka", "Thresh", "Zilean" };
-    private static string[] tank = new string[] { "Amumu", "Chogath", "DrMundo", "Galio", "Hecarim", "Malphite", "Maokai", "Nasus", "Rammus", "Sejuani", "Shen", "Singed", "Skarner", "Volibear", "Warwick", "Yorick", "Zac", "Nunu", "Taric", "Alistar", "Garen", "Nautilus" };
-    private static string[] ad = new string[] { "Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Graves", "KogMaw", "MissFortune", "Quinn", "Sivir", "Talon", "Tristana", "Twitch", "Urgot", "Varus", "Vayne", "Zed", "Jinx", "Yasuo", "Lucian"};
-    private static string[] bruiser = new string[] { "Darius", "Elise", "Evelynn", "Fiora", "Gangplank", "Jayce", "Pantheon", "Irelia", "JarvanIV", "Jax", "Khazix", "LeeSin", "Nocturne", "Olaf", "Poppy", "Renekton", "Rengar", "Riven", "Shyvana", "Trundle", "Tryndamere", "Udyr", "Vi", "MonkeyKing", "XinZhao", "Aatrox", "Rumble", "Shaco", "MasterYi" };
-
-    private Unit AutoPriority()
-    {
-        Unit autopriority = null;
-        int prio = 5;
-        foreach (Unit target in Unit.Heroes)
-        {
-            if (target != null && target.IsValid == true && target.IsEnemy == true && target.IsDead == false && target.IsVisible == true && Vector3.Distance(Unit.Player.Position, target.Position) <= _range)
-            {
-                if (autopriority == null)
-                {
-                    autopriority = target;
-                    prio = FindPrioForTarget(target.BaseSkinName);
-                }
-                else
-                {
-                    if (FindPrioForTarget(target.BaseSkinName) < prio)
-                    {
-                        autopriority = target;
-                        prio = FindPrioForTarget(target.BaseSkinName);
-                    }
-                    else if (FindPrioForTarget(target.BaseSkinName) == prio)
-                    {
-                        if (target.Health < autopriority.Health)
+                    case CardType.Blue:
+                        if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name == "bluecardlock")
                         {
-                            autopriority = target;
-                            prio = FindPrioForTarget(target.BaseSkinName);
+                            ObjectManager.Player.Spellbook.CastSpell(SpellSlot.W);
                         }
-                    }
+                        break;
+                    case CardType.Gold:
+                        if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name == "goldcardlock")
+                        {
+                            ObjectManager.Player.Spellbook.CastSpell(SpellSlot.W);
+                        }
+                        break;
+                    case CardType.Red:
+                        if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Name == "redcardlock")
+                        {
+                            ObjectManager.Player.Spellbook.CastSpell(SpellSlot.W);
+                        }
+                        break;
                 }
             }
         }
-        return autopriority;
-    }
-
-    private static int FindPrioForTarget(string baseskinname)
-    {
-        if (ap.Contains(baseskinname))
-        {
-            return 2;
-        }
-        else if (ad.Contains(baseskinname))
-        {
-            return 1;
-        }
-        else if (sup.Contains(baseskinname))
-        {
-            return 3;
-        }
-        else if (bruiser.Contains(baseskinname))
-        {
-            return 4;
-        }
-        else if (tank.Contains(baseskinname))
-        {
-            return 5;
-        }
-        else
-        {
-            return 5;
-        }
-    }
-
-    public void setDrawCircleOfTarget(bool draw)
-    {
-        drawcircle = draw;
-    }
-
-    public void OverrideTarget(Unit newtarget)
-    {
-        target = newtarget;
-        update = false;
-    }
-
-    public void DisableTargetOverride()
-    {
-        update = true;
-    }
-
-    public float getRange()
-    {
-        return _range;
-    }
-
-    public void setRange(float range)
-    {
-        _range = range;
-    }
-
-    public TargetingMode getTargetingMode()
-    {
-        return _mode;
-    }
-
-    public void setTargetingMode(TargetingMode mode)
-    {
-        _mode = mode;
-    }
-
-    public override string ToString()
-    {
-        return "Target: " + target.BaseSkinName + "Range: " + _range.ToString() + "Mode: " + _mode.ToString();
     }
 }
